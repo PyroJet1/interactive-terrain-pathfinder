0d9a4906c9401c447a6966f7d77d0bbb8d1f4529
alan edwin <alansedwin@gmail.com>
2026-02-20 10:28:01 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const canvas = document.getElementById('map');
+const ctx = canvas.getContext('2d');
+const regenerateBtn = document.getElementById('regenerate');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+regenerateBtn.addEventListener('click', () => {
+});
+
+canvas.addEventListener('click', (e) => {
+});
+
+canvas.addEventListener('mousemove', (e) => {
+});
