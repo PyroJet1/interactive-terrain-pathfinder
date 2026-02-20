a886e69ce4c42c79b8e5bfb057f04e011dd059b5
alan edwin <alansedwin@gmail.com>
2026-02-20 12:23:41 +0100

    advanced terrain generation, added gradients

--- script.js
+++ script.js
@@ -3,42 +3,47 @@ let gridHeight = 150;
 let cellWidth, cellHeight;
 let zoomFactor = 0.05;
 
+let WATER, SHORE, GRASS, MOUNTAIN, SNOW; 
+
 function setup(){
     createCanvas(windowWidth, windowHeight);
     cellWidth = width / gridWidth;
     cellHeight = height / gridHeight;
+
+    noiseDetail(7, 0.5)
+
+    WATER = new Terrain('water', color(64, 123, 158), 10);
+    SHORE = new Terrain('shore', color(238, 214, 175), 2);
+    GRASS = new Terrain('grass', color(139, 172, 120), 1);
+    MOUNTAIN = new Terrain('mountain', color(169, 157, 144), 5);
+    SNOW = new Terrain('snow', color(255, 255, 255), 8)
+
     background(200);
     noLoop();
-    redraw();
+
+    console.log('Terrain initialized:', WATER, SHORE, GRASS, MOUNTAIN, SNOW);
+}
+
+function getTerrainType(noiseValue) {
+    if (noiseValue < 0.4) return {terrain: WATER, min: 0, max: 0.4};
+    if (noiseValue < 0.5) return {terrain: SHORE, min: 0.4, max: 0.5};
+    if (noiseValue < 0.65) return {terrain: GRASS, min: 0.5, max: 0.65};
+    if (noiseValue < 0.8) return {terrain: MOUNTAIN, min: 0.65, max: 0.8};
+    return {terrain: SNOW, min: 0.8, max: 1.0};
 }
 
 function draw(){
+    
     for (let x = 0; x < gridWidth; x++){
         for (let y = 0; y < gridHeight; y++){
             const noiseValue = noise(x * zoomFactor, y * zoomFactor);
-            
-            let terrainColor;
-            if (noiseValue < 0.4){
-                terrainColor = color(64, 123, 158);
-            }
-            else if (noiseValue < 0.5){
-                terrainColor = color(238, 214, 175);
-            }
-            else if (noiseValue < 0.65){
-                terrainColor = color(139, 172, 120);
-            }
-            else if (noiseValue < 0.8){
-                terrainColor = color(169, 157, 144);
-            }
-            else{
-                terrainColor = color(255,255,255);
-            }
-
-            fill(terrainColor);
+            const terrainData = getTerrainType(noiseValue);
+            fill(terrainData.terrain.getColor(noiseValue, terrainData.min, terrainData.max));
             noStroke();
             rect(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
         }
     }
+    
 }
 
 function mouseClicked(){
@@ -46,7 +51,3 @@ function mouseClicked(){
 
 function mouseMoved(){
 }
-
-// function regenerateMap() {
-//   redraw();
-// }
---
+++ terrain.js
@@ -0,0 +1,18 @@
+
+class Terrain {
+    constructor(name, baseColor, cost){
+        this.name = name;
+        this.baseColor = baseColor;
+        this.cost = cost;
+    }
+
+    getColor(noiseValue, minThreshold, maxThreshold){
+        let r = red(this.baseColor);
+        let g = green(this.baseColor);
+        let b = blue(this.baseColor);
+
+        let variation = map(noiseValue, minThreshold, maxThreshold, 0.75, 1.25);
+
+        return color(r * variation, g * variation, b * variation);
+    }
+}
