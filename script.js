e28f44beb125ae6a0f6a083e836ce9ba5325315e
alan edwin <alansedwin@gmail.com>
2026-02-20 13:54:36 +0100

    markers implemented

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-let gridWidth = 400;
-let gridHeight = 300;
+let gridWidth = 200;
+let gridHeight = 150;
 let cellWidth, cellHeight;
 let zoomFactor = 0.04;
 
-let WATER, SHORE, GRASS, MOUNTAIN, SNOW; 
+let sourcePoint = null
+let destPoint = null
+
+let WATER, SHORE, GRASS, MOUNTAIN, SNOW;
+let terrainLayer;
+let destLocked;
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
@@ -18,8 +23,11 @@ function setup(){
     MOUNTAIN = new Terrain('mountain', color(169, 157, 144), 5);
     SNOW = new Terrain('snow', color(255, 255, 255), 8)
 
+    terrainLayer = createGraphics(width, height);
+    drawTerrain();
+
     background(200);
-    noLoop();
+    loop();
 
     console.log('Terrain initialized:', WATER, SHORE, GRASS, MOUNTAIN, SNOW);
 }
@@ -32,8 +40,7 @@ function getTerrainType(noiseValue) {
     return {terrain: SNOW, min: 0.8, max: 1.0};
 }
 
-function draw(){
-    
+function drawTerrain(){
     for (let x = 0; x < gridWidth; x++){
         for (let y = 0; y < gridHeight; y++){
 
@@ -52,21 +59,67 @@ function draw(){
             
             let terrainColor = terrainData.terrain.getColor(noiseValue, terrainData.min, terrainData.max);
             
-            fill(
+            terrainLayer.fill(
                 red(terrainColor) * constrainedLighting, 
                 green(terrainColor) * constrainedLighting, 
                 blue(terrainColor) * constrainedLighting
             );
+
             
-            noStroke();
-            rect(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
+            terrainLayer.noStroke();
+            terrainLayer.rect(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
         }
+    }
+}
+
+function draw(){
+    
+    image(terrainLayer, 0, 0);
+
+    if (sourcePoint){
+        drawMarker(sourcePoint.x, sourcePoint.y, color(255,0,0));
+    }
+    if (destPoint){
+        drawMarker(destPoint.x, destPoint.y, color(0,0,255));
+    } 
+}
+
+function drawMarker(gridX, gridY, markerColor){
+    fill(markerColor);
+    stroke(255);
+    strokeWeight(2);
+    ellipse(
+        gridX * cellWidth + cellWidth/2, 
+        gridY * cellHeight + cellHeight/2, 
+        cellWidth * 2, 
+        cellHeight * 2
+    );
+}
+
+function mouseClicked(){
+    let gridX = floor(mouseX / cellWidth);
+    let gridY = floor(mouseY / cellHeight);
+
+    if (gridX >= 0 && gridX < gridWidth && gridY >= 0 && gridY < gridHeight){
+        if(sourcePoint == null){
+            sourcePoint = {x: gridX, y: gridY};
+        }
+        else{
+            destPoint = {x: gridX, y: gridY};
+            destLocked = true;
+            console.log("DestPoint:", gridX, " ", gridY);
+        } 
     }
     
 }
 
-function mouseClicked(){
-}
-
 function mouseMoved(){
+    if (sourcePoint && !destLocked) {
+        let gridX = floor(mouseX / cellWidth);
+        let gridY = floor(mouseY / cellHeight);
+        
+        if (gridX >= 0 && gridX < gridWidth && gridY >= 0 && gridY < gridHeight) {
+            destPoint = {x: gridX, y: gridY};
+        }
+    }
 }
